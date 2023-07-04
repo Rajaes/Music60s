38078a5894dcbc6054efb9314e83211a2bfc5fca
Rajaes <rajaessolhe@gmail.com>
2023-07-04 13:45:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,218 @@
+/*
+design by Voicu Apostol.
+design: https://dribbble.com/shots/3533847-Mini-Music-Player
+I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
+You can fork on github: https://github.com/muhammederdem/mini-player
+*/
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
+        {
+          name: "Time",
+          artist: "Hans Zimmer",
+          cover: "https://i.pinimg.com/originals/72/a1/ec/72a1ecd69721754925cbc9bf64c5f007.jpg",
+          source: "https://drive.google.com/uc?export=download&id=1yxAqXEKv-hsYatcrhEblgTEOvq6TAlBA",
+          url: "https://youtu.be/c56t7upa8Bk",
+          favorited: false
+        },
+        {
+          name: "Everybody Knows",
+          artist: "Leonard Cohen",
+          cover: "https://i.pinimg.com/originals/d7/d5/e2/d7d5e27935e5e7b20956e341013a0138.jpg",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+          favorited: true
+        },
+        {
+          name: "Dear old friend",
+          artist: "XY",
+          cover: "https://i.pinimg.com/originals/9f/9b/7f/9f9b7f2309e1860bca793e28d02920a1.gif",
+          source: "https://drive.google.com/uc?export=download&id=10NSxUrdduYfug-3zy8jB_2tx5Q_azo6g",
+          url: "https://www.youtube.com/watch?v=oCgVbk7OqHY",
+          favorited: false
+        },
+        {
+          name: "Slow Down",
+          artist: "Chase Atlantic",
+          cover: "https://i.pinimg.com/originals/5f/7a/60/5f7a60591286779152c35443d3b44014.gif",
+          source: "https://drive.google.com/uc?export=download&id=10dT8Hg7JJrKDG5pMamlWnWcEjqi4V-vE",
+          url: "https://youtu.be/s_y-BbYQ7Bc",
+          favorited: false
+        },
+        {
+          name: "The Final Victory",
+          artist: "Haggard",
+          cover: "https://i.pinimg.com/originals/d1/72/71/d17271b22bf59c2fb3ef470071a96228.gif",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+          url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+          favorited: true
+        },
+        {
+          name: "Genius ft. Sia, Diplo, Labrinth",
+          artist: "LSD",
+          cover: "https://i.pinimg.com/originals/fc/53/08/fc5308548f9ffacac8bb1d482620da63.gif",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+          url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+          favorited: false
+        },
+        {
+          name: "The Comeback Kid",
+          artist: "Lindi Ortega",
+          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+          url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
+          favorited: true
+        },
+        {
+          name: "Overdose",
+          artist: "Grandson",
+          cover: "https://i.pinimg.com/originals/73/4f/c1/734fc1a60173d00e6f7fdc39843ba33a.gif",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+          url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
+          favorited: false
+        },
+        {
+          name: "Human",
+          artist: "Rag´n bon man",
+          cover: "https://i.pinimg.com/originals/c7/77/3f/c7773f2062ab779347e0b214f4bc52de.gif",
+          source: "",
+          url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+          favorited: false
+        }
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
+    }
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+
+    // this is optional (for preload covers)
+    for (let index = 0; index < this.tracks.length; index++) {
+      const element = this.tracks[index];
+      let link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image"
+      document.head.appendChild(link)
+    }
+  }
+});
